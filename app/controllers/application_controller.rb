91856d9b8a230f7f51923babc2d65f3a989888a8
Yu Abe <y0u4a2b6e0@gmail.com>
2019-06-03 15:54:57 +0900

    Add categories model and tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
     
     def current_user
-        #まずログインユーザーがいるか確認する、いなければ、以下のように定義
+        #まずログインユーザーがいるか確認する、いなければ、以下のように定義 もしセッションにuse_idがあれば、セッション内のuser_idを頼りにfind
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
     
-    # !!とメソッド名? はセット。「ログインしてるか？」
+    # !!とメソッド名? はセット。「ログインしてるか？」 もしcurrent_userがなければ
     def logged_in?
         !!current_user
     end
---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ApplicationRecord
+    validates :name, presence: true, length: { 
+        #minimum: 1,
+        maximum: 25 }
+    validates_uniqueness_of :name
+end
